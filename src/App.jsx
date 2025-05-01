704d89653d56ed327e391f32058d09b38fa01989
Roman De Rose <romandr37@gmail.com>
2025-05-01 18:18:43 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1 className="text-red-500 text-4xl">hola</h1>
+    </div>
+  );
+}
+
+export default App;
